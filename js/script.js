4b75ae234f3950a4497842f7a1aa47c4d656510a
Decker Wellington <deckerwellington@yahoo.com>
2022-08-06 03:05:18 +0100

    I made some changes for the mobile nav

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const burgerBtn = document.querySelector(".hamburger");
 const closeBtn = document.querySelector(".closeBtn");
 const mobileNav = document.querySelector(".mobile-nav");
-
-
+const body = document.querySelector("body")
 
 
 burgerBtn.addEventListener('click', () => {
     mobileNav.classList.add("display");
     burgerBtn.style.display = "none"
     closeBtn.style.display = "block"
+    body.style.overflow = "hidden";
+    
 });
 
 closeBtn.addEventListener('click', () => {
     mobileNav.classList.remove("display");
     burgerBtn.style.display = "block"
     closeBtn.style.display = "none"
+    body.style.overflow = "auto";
 });
 
